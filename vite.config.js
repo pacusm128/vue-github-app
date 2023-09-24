0ce96472f606676814cc86d3d47e3be249b453a6
Patipol Ch <pacusm128@gmail.com>
2023-09-24 16:40:04 +0700

    x

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-githum-app/',
+  base: '/vue-githubapp/',
   plugins: [vue()],
   resolve: {
     alias: {
